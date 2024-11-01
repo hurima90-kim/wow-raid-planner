5028d803426403e17113728134fc07dc9acf685a
hurima90-kim <hurima90@gmail.com>
2024-11-01 16:42:39 +0900

    feat: write comment 'how to run'

--- src/index.js
+++ src/index.js
@@ -25,3 +25,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// npm run-script react-start
+// npm run electron-start
